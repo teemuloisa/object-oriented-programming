4eccbd7d1b57b934ce3e57fd461df0778122d987
Teemu Loisa <teemu.loisa@gmail.com>
2018-01-11 11:02:17 +0200

    task-person tehty

---
+++ object-method/task-person/task-person/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_person
+{
+    class Person
+    {
+        //Fields
+        public string Name;
+        private int age;
+
+        //Constructor
+        public Person()
+        {
+            Name = "unknown";
+            age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+
+        //Methods
+        public void GrowOld()
+        {
+            this.age++;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {Name}\nIkä: {this.age}\nAikuinen: {IsAdult()}");
+        }
+
+        public bool IsAdult()
+        {
+           // return this.age >= 18;
+
+            if (this.age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+    }
+}
---
+++ object-method/task-person/task-person/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personJouni = new Person();
+            personJouni.Name = "Jouni K";
+            personJouni.GrowOld();
+            personJouni.PrintPersonInfo();
+
+            Person newPerson = new Person("Joe Doe", 17);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+            Console.ReadKey();
+        }
+    }
+}
