cac1e7942aa361d5b6ba358012ffc3ed2f8f0e49
Teemu Loisa <teemu.loisa@gmail.com>
2018-01-12 09:28:48 +0200

    class-book tehty

---
+++ object-method/task-book/task-book/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_book
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Id;
+        public int Price;
+
+        public Book(string title, string author, int id, int price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+
+            
+        }
+
+        public string CompareBook(Book book)
+        {
+            
+            if (this.Price > book.Price)
+                return( $"{this.Title} on kalliimpi kuin {book.Title} kirja");
+            else
+                return ($"{book.Title} on kalliimpi kuin {this.Title} kirja");
+        }
+
+       
+    }
+}
--- object-method/task-book/task-book/Program.cs
+++ object-method/task-book/task-book/Program.cs
@@ -6,7 +6,18 @@ namespace task_book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book book1 = new Book("Kirja 1", "Kirjailija 1", 123, 10);
+            Book book2 = new Book("Kirja 2", "Kirjailija 2", 124, 15);
+
+            Console.WriteLine($"Vertailu: {book1.CompareBook(book2)}");
+
+            Console.ReadKey();
+
+
+
+
         }
+
+     
     }
 }
