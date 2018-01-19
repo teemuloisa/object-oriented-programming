8742b55dbafee37e3da1d88f534c616a676a98aa
Teemu Loisa <teemu.loisa@gmail.com>
2018-01-19 09:41:56 +0200

    task-car tehty.

---
+++ object-method/task-car/task-car/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_car
+{
+    class Car
+    {
+        public string Brand;
+        public int Speed;
+
+
+        public Car()
+        {
+            Brand = "unknow";
+            Speed = 0;
+        }
+
+
+        public Car(string brand, int speed)
+        {
+            Brand = brand;
+            Speed = speed;
+        } 
+        
+
+        public void AskData()
+        {
+            Console.Write("Input car brand: ");
+            string userInput = Console.ReadLine();
+            Brand = userInput;
+            Console.Write("Input car speed: ");
+            string userInput1 = Console.ReadLine();
+            Speed = int.Parse(userInput1);
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}\nSpeed: {Speed}");
+        }
+
+        public void Accelerate(int number)
+        {
+            Speed = Speed + number;
+        }
+
+        public void Brake(int number)
+        {
+            Speed = (Speed * number) / 100;
+        }
+
+
+    }
+}
---
+++ object-method/task-car/task-car/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car carAuto1 = new Car();
+            carAuto1.AskData();
+            carAuto1.ShowCarInfo();
+            Console.WriteLine("Car speed after 40+:");
+            carAuto1.Accelerate(40);
+            carAuto1.ShowCarInfo();
+            Console.WriteLine("Car speed after 10% slowdown:");
+            carAuto1.Brake(90);
+            carAuto1.ShowCarInfo();
+
+            Car carAuto2 = new Car();
+            carAuto2.AskData();
+            carAuto2.ShowCarInfo();
+            Console.WriteLine("Car speed after 40+:");
+            carAuto2.Accelerate(40);
+            carAuto2.ShowCarInfo();
+            Console.WriteLine("Car speed after 10% slowdown:");
+            carAuto2.Brake(90);
+            carAuto2.ShowCarInfo();
+
+            Console.ReadKey();
+
+
+
+        }
+
+     }
+}
